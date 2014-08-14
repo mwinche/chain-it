dd8e16e4ece26e332de416efd7e8804f8fdf10b4
Matt Winchester <matthewwinchester@attask.com>
2014-08-14 12:44:57 -0600

    expanded from and source functionality

--- test/fromSpec.js
+++ test/fromSpec.js
@@ -23,6 +23,38 @@ describe('from', function(){
 			it('should be a Source instance', function(){
 				expect(from.src('{1}.js').constructor).toBe(Source);
 			});
+
+			it('should have the same source descriptor', function(){
+				expect(from.src('{1}.js').sourceDescription).toBe('{1}.js');
+			});
+		});
+	});
+
+	describe('resource function', function(){
+		it('should be a function', function(){
+			expect(typeof from.resource).toBe('function');
+		});
+
+		it('should not allow no arguments', function(){
+			expect(function(){
+				from.resource();
+			}).toThrow();
+		});
+
+		it('should not allow two arguments', function(){
+			expect(function(){
+				from.resource(1,2);
+			}).toThrow();
+		});
+
+		describe('return value', function(){
+			it('should be a Source instance', function(){
+				expect(from.resource('{1}.js').constructor).toBe(Source);
+			});
+
+			it('should have the same source descriptor', function(){
+				expect(from.src('{1}.js').sourceDescription).toBe('{1}.js');
+			});
 		});
 	});
 });
