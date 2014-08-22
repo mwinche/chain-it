3796ac9d9f16fe8562ab7ed48c16f2e4b1f978ff
Matt Winchester <matthewwinchester@attask.com>
2014-08-21 23:10:56 -0600

    wrote up CLI, broke tests in the process as it was needed to adapt to the nature of gulp, test need to be fixed

--- cli.js
+++ cli.js
@@ -2,15 +2,43 @@
 
 'use strict';
 
-var chainLink = require('./chain-link');
+var chain = require('./lib/chain'),
+	path = require('path'),
+	resources = require(path.join(process.cwd(), 'chain.js')),
+	express = require('express'),
+	gulp = require('gulp');
 
 var userArgs = process.argv;
-var searchParam = userArgs[2];
 
-if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1 || searchParam === undefined) {
+var app = express();
+
+if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1) {
 	return console.log('cli help');
 }
 
 if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
 	return console.log(require('./package').version);
 }
+
+app.get('*', function(req, res){
+	var result = resources.get(req.path);
+
+	if(result){
+		var source = gulp.src(path.join(process.cwd(), result.resolve())),
+			stream = result.source().compile(source);
+
+		stream.on('data', function(data){
+			data.pipe(res);
+		})
+			.on('error', function(err){
+				console.log(err);
+			});
+	}
+	else{
+		res.status(404).end('Not Found');
+	}
+});
+
+app.listen(3000);
+
+
